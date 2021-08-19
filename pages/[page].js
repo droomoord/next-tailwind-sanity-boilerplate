783d889f5e72995c0f3c94da06e8d080743ee131
Kris <heijnenkris@gmail.com>
2021-08-19 18:57:54 +0200

    commit

--- pages/[page].js
+++ pages/[page].js
@@ -1,10 +1,10 @@
 import BlockContent from "@sanity/block-content-to-react";
-import { fetchPage, fetchPageTitles } from "../functions";
+import { fetchPage } from "../functions";
+import LazyLoad from "react-lazyload";
 import Head from "next/head";
 
 export default function Home(props) {
   const { page } = props;
-  console.log(page);
 
   const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
   const dataset = process.env.NEXT_PUBLIC_DATASET;
@@ -27,18 +27,21 @@ export default function Home(props) {
         {metatags}
       </Head>
 
-      <BlockContent
-        className="flex flex-col"
-        blocks={page.body}
-        projectId={projectId}
-        dataset={dataset}
-      ></BlockContent>
+      <LazyLoad>
+        <BlockContent
+          className="flex flex-col"
+          blocks={page.body}
+          projectId={projectId}
+          dataset={dataset}
+        ></BlockContent>
+      </LazyLoad>
     </>
   );
 }
 
-export async function getStaticProps(context) {
+export async function getServerSideProps(context) {
   const { data } = await fetchPage(context.params.page);
+  console.log("context", context.params.page);
 
   if (!data || data.length < 1) {
     return {
@@ -48,20 +51,34 @@ export async function getStaticProps(context) {
   const page = data[0];
   return {
     props: { page },
-    revalidate: 10,
   };
 }
 
-export async function getStaticPaths() {
-  const { data } = await fetchPageTitles();
-  return {
-    paths: data.map((page) => {
-      return {
-        params: {
-          page: page.slug,
-        },
-      };
-    }),
-    fallback: "blocking",
-  };
-}
+// export async function getStaticProps(context) {
+//   const { data } = await fetchPage(context.params.page);
+
+//   if (!data || data.length < 1) {
+//     return {
+//       notFound: true,
+//     };
+//   }
+//   const page = data[0];
+//   return {
+//     props: { page },
+//     revalidate: 10,
+//   };
+// }
+
+// export async function getStaticPaths() {
+//   const { data } = await fetchPageTitles();
+//   return {
+//     paths: data.map((page) => {
+//       return {
+//         params: {
+//           page: page.slug,
+//         },
+//       };
+//     }),
+//     fallback: "blocking",
+//   };
+// }
